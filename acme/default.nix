2656aba957ed3eb9ed65cd84bd01d885c6d08654
Hallam Smith <me@hcssmith.com>
2024-09-17 18:09:20 +0100

    fix(acme): fix typo

--- acme/default.nix
+++ acme/default.nix
@@ -4,20 +4,21 @@
   browser = "firefox";
   font = "CMUTypewriter-Regular";
   size = "22a";
-in pkgs.writeScriptBin "acme2"
-''
-#!${pkgs.nushell}/bin/nu
-def main [...args] {
-	with-env {
-		SHELL:${shell},
-		acmeshell:${shell},
-		BROWSER:${browser},
-		tabstop:2,
-		TERM:dumb,
-		PAGER:nobs
-	} {
-		let _args = if ($args | is-empty) {"."} else {$args | str join ' '}
-		${p9} acme -a -f /mnt/font/${font}/${size}/font $_args
-	}
-}
-''
+in
+  pkgs.writeScriptBin "acme"
+  ''
+    #!${pkgs.nushell}/bin/nu
+    def main [...args] {
+    	with-env {
+    		SHELL:${shell},
+    		acmeshell:${shell},
+    		BROWSER:${browser},
+    		tabstop:2,
+    		TERM:dumb,
+    		PAGER:nobs
+    	} {
+    		let _args = if ($args | is-empty) {"."} else {$args | str join ' '}
+    		${p9} acme -a -f /mnt/font/${font}/${size}/font $_args
+    	}
+    }
+  ''
